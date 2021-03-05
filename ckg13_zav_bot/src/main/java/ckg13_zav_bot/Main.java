ff257509aa0fb6164b32d41e08a3733723264941
Yulia-st <79211395+Yulia-st@users.noreply.github.com>
2021-03-05 02:08:42 +0200

    Updated Main.java

--- ckg13_zav_bot/src/main/java/ckg13_zav_bot/Main.java
+++ ckg13_zav_bot/src/main/java/ckg13_zav_bot/Main.java
@@ -8,26 +8,12 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 public class Main {
 public static void main(String[] args) throws TelegramApiException{
-	//ApiContextInitializer.init();
-//	System.getProperties().put("proxySet", "true");
-//	System.getProperties().put("socketProxyHost", "127.0.0.1");
-//	System.getProperties().put("socketProxyHost", "9150");
 	
 	TelegramBotsApi api = new TelegramBotsApi(DefaultBotSession.class);
 	api.registerBot(new Bot());
 	System.out.println("Start");
 
-/*	
-	try {
-	String s = Weather.getWeather("London");
-	System.out.println(s);
-	}
-	catch (IOException e1){
-		System.out.println("Города такого нет");
-		e1.printStackTrace();
-		
-	}
-*/	
+
 	
 }
 }
